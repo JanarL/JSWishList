52e782f542c11e8a714c8739990af1b73855fe7f
janar liiv <janar.liiv@hotmail.com>
2020-04-22 11:42:30 +0300

    done

---
+++ controllers/wishes.js
@@ -0,0 +1,27 @@
+const Wish = require('../models/wish');
+exports.getAddWishPage =
+(req, res) => {
+    res.render('add-wish', {
+        pageTitle: "Add New wish",
+        path: "/admin/add-wish"
+    });
+}
+exports.postAddWishes = 
+(req, res) => {
+        console.log(req.body.wish);
+        const wish = new Wish(req.body.wish);
+        wish.saveWish();
+        res.redirect('/');
+    };
+exports.getWishes =
+(req, res) => {
+        Wish.fetchAllWishes((wishes)=>{
+           res.render('wishList', {
+            pageTitle: 'Welcome to My WishList!',
+            wishes: wishes,
+            path: '/'
+        });   
+    })       
+
+};
+
